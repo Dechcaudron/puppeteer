206b2b7b2ee6a2a286f7cf4bac6816cdabdcc796
Héctor Barreras Almarcha <hbaelx@gmail.com>
2016-06-20 23:26:52 +0200

    Listener count for SignalWrapper fixed

--- source/puppetteer/signal_wrapper.d
+++ source/puppetteer/signal_wrapper.d
@@ -2,23 +2,19 @@ module puppeteer.signal_wrapper;
 
 import puppeteer.signal;
 import std.stdio;
-import core.atomic : atomicOp;
 
 //TODO: life would be so much easier if this class could be shared :)
 package synchronized class SignalWrapper(T1...)
 {
     private shared mixin Signal!T1 signal;
-    private shared int _listeners = 0;
 
     public void addListener(void delegate(T1) shared listener) shared
     {
-        _listeners.atomicOp!"+="(1);
         signal.connect(listener);
     }
 
     public void removeListener(void delegate(T1) shared listener) shared
     {
-        _listeners.atomicOp!"-="(1);
         signal.disconnect(listener);
     }
 
@@ -28,8 +24,8 @@ package synchronized class SignalWrapper(T1...)
     }
 
     @property
-    public int listenersNumber() shared
+    public size_t listenersNumber() shared
     {
-        return _listeners;
+        return slots_idx;
     }
 }
