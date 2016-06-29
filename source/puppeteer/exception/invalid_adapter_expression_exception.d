9facbf39bc7e70562982b219331c6b2403057a2d
Héctor Barreras Almarcha <hbaelx@gmail.com>
2016-06-29 08:35:25 +0200

    Fixed InvalidAdapterExpressionException placement

--- source/puppeteer/exception/invalid_adapter_expression_exception.d
+++ source/puppeteer/exception/invalid_adapter_expression_exception.d
@@ -1,3 +1,5 @@
+module puppeteer.exception.invalid_adapter_expression_exception;
+
 public class InvalidAdapterExpressionException : Exception
 {
     this(string msg, string file = __FILE__, size_t line = __LINE__)
--- source/puppetteer/exception/invalid_adapter_expression_exception.d
+++
@@ -1,9 +0,0 @@
-module puppeteer.exception.invalid_adapter_expression_exception;
-
-public class InvalidAdapterExpressionException : Exception
-{
-    this(string msg, string file = __FILE__, size_t line = __LINE__)
-    {
-        super(msg, file, line, null);
-    }
-}
